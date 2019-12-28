80ffc9095b0c7e2eedb37a50f23ba5cb3df02be4
siste <sistemadesignstyle@gmail.com>
2019-12-27 20:01:21 -0500

    layout con bootstrap

---
+++ resources/views/inicio/inicio.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 offset-md-4">
+            <h1>Texto</h1>
+        </div>
+    </div>
+</div>
+
+@stop
+
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lan="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+   
+    <link rel="stylesheet" href="{{ asset('recursos/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('recursos/css/estilo.css') }}">
+   
+    <title>Inicio</title>
+  </head>
+  <body>
+ 
+    @yield('content')
+
+    <script src="{{ asset('recursos/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('recursos/bootstrap/js/bootstrap.min.js') }}"></script>
+   
+  </body>
+</html>
